7689bdae40afb172693b50ea0b6131e813aea3e7
Rochelle Rebello <mariaroch9@gmail.com>
2022-09-10 10:11:53 -0400

    Adding the election audit code.

--- Python_practice.py
+++ Python_practice.py
@@ -21,21 +21,4 @@ for num in range(5):
 for i in range(len(counties)):
     print(counties [i])
 
-# Add our dependencies.
-import csv
-import os
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
-file_to_save = os.path.join("..", "Analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-    # To do: read and analyze the data here.
-
-   # Read the file object with the reader function.
-    file_reader = csv.reader(election_data)
-# Print each row in the CSV file.
-for row in file_reader:
-    print(row)
